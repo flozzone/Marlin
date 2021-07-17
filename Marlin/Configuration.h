c1fa257af19ea4682a8f7b2a2bc60182a04938cb
flozzone <flozzone@gmail.com>
2021-07-17 07:27:43 +0200

    Config: Start with the Left-Back Corner when doing corner leveling.
    The left-back corner on the Ender-3 is the one, where the bed-heat cable
    mount is mounted with the screw that holds the bed. Since this screw
    is kind of fixed every manual bed leveling starts from that corner.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1708,7 +1708,7 @@
    *  |  1       2  |   | 1         4 |    | 1         2 |   | 2           |
    *  LF --------- RF   LF --------- RF    LF --------- RF   LF --------- RF
    */
-  #define LEVEL_CORNERS_LEVELING_ORDER { LF, RF, RB, LB }
+  #define LEVEL_CORNERS_LEVELING_ORDER { LB, LF, RF, RB }
 #endif
 
 /**
