adde7874e4f8765d52712da56af438a830d70e4f
flozzone <flozzone@gmail.com>
2021-07-18 21:23:10 +0200

    Config: Let the nozzle park outside of the bed.
    When the nozzle is heating up, filament is flowing freely from the nozzle.
    Especially with PETG material. By parking the nozzle outside of the bed
    the filament doesn't fall on the bed and can fall off.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1375,10 +1375,10 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 0
+#define Y_MIN_POS -40
 #define Z_MIN_POS 0
 #define X_MAX_POS (X_BED_SIZE + 50)
-#define Y_MAX_POS Y_BED_SIZE
+#define Y_MAX_POS (Y_BED_SIZE - 40)
 #define Z_MAX_POS 250
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50
@@ -1883,7 +1883,7 @@
 
 #if ENABLED(NOZZLE_PARK_FEATURE)
   // Specify a park position as { X, Y, Z_raise }
-  #define NOZZLE_PARK_POINT { (X_MIN_POS + 10), (Y_MAX_POS - 10), 20 }
+  #define NOZZLE_PARK_POINT { (X_MIN_POS + 0), (Y_MIN_POS - 0), 20 }
   //#define NOZZLE_PARK_X_ONLY          // X move only is required to park
   //#define NOZZLE_PARK_Y_ONLY          // Y move only is required to park
   #define NOZZLE_PARK_Z_RAISE_MIN   2   // (mm) Always raise Z by at least this distance
